2fadfc8a3740ca0d8ae0b3577ddc7e58ef683c58
Anne L'Hôte <anne.lhote@gmail.com>
2023-06-26 14:36:54 +0200

    feat(graph): Display graph on client via sigma

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,5 +1,8 @@
+import '@react-sigma/core/lib/react-sigma.min.css';
 import { Container, Text, Title } from '@dataesr/react-dsfr';
 import { useQuery } from '@tanstack/react-query';
+import { MultiDirectedGraph } from 'graphology';
+import { SigmaContainer } from '@react-sigma/core';
 
 async function getHello() {
   return fetch('/api/hello').then((response) => {
@@ -8,12 +11,18 @@ async function getHello() {
   });
 }
 
+const graph = new MultiDirectedGraph();
+graph.addNode('A', { x: 0, y: 0, label: 'Node A', size: 10 });
+graph.addNode('B', { x: 1, y: 1, label: 'Node B', size: 10 });
+graph.addEdgeWithKey('rel1', 'A', 'B', { label: 'REL_1' });
+
 export default function Home() {
   const { data, isLoading } = useQuery({ queryKey: ['hello'], queryFn: getHello });
   return (
     <Container className="fr-my-15w">
       <Title as="h1">Doadify</Title>
       <Text>{isLoading ? 'Chargement...' : data?.hello}</Text>
+      <SigmaContainer style={{ height: '500px' }} graph={graph} />
     </Container>
   );
 }
