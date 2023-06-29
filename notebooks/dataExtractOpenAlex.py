9cd70ed1749caf9c3defed628c749b58362cd9ca
juliaGrandhay <julia.grandhay@gmail.com>
2023-06-29 12:16:28 +0200

    add display name and orcid to graph

--- notebooks/dataExtractOpenAlex.py
+++ notebooks/dataExtractOpenAlex.py
@@ -35,6 +35,7 @@ for i in range(len(res['results'])):
         dic = res["results"][i]["authorships"][l]["author"]
         dic["doi"] = res["results"][i]["doi"]
         dic["title"] = res["results"][i]["title"]
+        dic["id_pub"] = res["results"][i]["id"]
         for key in keys:
             if key not in ["author", "institutions", "raw_affiliation_strings"]:
                 dic[key] = res["results"][i]["authorships"][l][key]
@@ -60,6 +61,7 @@ for page in range(2, nb_page + 1):
             dic = res["results"][i]["authorships"][l]["author"]
             dic["doi"] = res["results"][i]["doi"]
             dic["title"] = res["results"][i]["title"]
+            dic["id_pub"] = res["results"][i]["id"]
             for key in keys:
                 if key not in ["author", "institutions", "raw_affiliation_strings"]:
                     dic[key] = res["results"][i]["authorships"][l][key]
@@ -76,12 +78,25 @@ for page in range(2, nb_page + 1):
             authors.append(dic)
 data = pd.DataFrame(data=authors)
 
+pub_compte = data[["id_pub", "id"]].groupby("id_pub").nunique().reset_index().rename(columns={"id": "compte_auth"})
+aut_compte = data[["id_pub", "id"]].groupby("id").nunique().reset_index().rename(columns={"id_pub": "compte_pub"})
+
+pub_compte2 = pub_compte.loc[pub_compte["compte_auth"] > 1].reset_index().drop(columns="index")
+aut_compte2 = aut_compte.loc[aut_compte["compte_pub"] > 1].reset_index().drop(columns="index")
+
+data2 = data.loc[(data["id_pub"].isin(pub_compte2["id_pub"])) & (data["id"].isin(aut_compte2["id"]))]
+
+compte = data2[["id", "id_pub"]].groupby("id_pub").nunique().reset_index().rename(columns={"id": "compte"})
+compte = compte.loc[compte["compte"] > 1]
+
+data3 = data2.loc[data2["id_pub"].isin(compte["id_pub"])]
+
 co = {"source": [], "target": []}
 co_liste = []
-data["id_doi"] = data["title"] + data["doi"].astype(str)
-for auth in set(data["id"]):
-    pub = list(data.loc[data["id"]==auth, "id_doi"])
-    coa = list(data.loc[data["id_doi"].isin(pub), "id"])
+
+for auth in set(data3["id"]):
+    pub = list(data3.loc[data3["id"]==auth, "id_pub"])
+    coa = list(data3.loc[data3["id_pub"].isin(pub), "id"])
     for item in coa:
         if item != auth:
             liste = [auth, item]
@@ -94,8 +109,14 @@ for auth in set(data["id"]):
 coauth = pd.DataFrame(data=co)
 coauth = coauth.drop_duplicates()
 
+data_auth = data2[['id', 'display_name', 'orcid']].drop_duplicates()
+data_auth = data_auth.rename(columns={"id": "source"})
+
+coauth2 = pd.merge(coauth, data_auth, on="source", how="left")
+coauth2 = coauth2.fillna("")
+
 G = nx.Graph()
 
-G = nx.from_pandas_edgelist(coauth, 'source', 'target')
+G = nx.from_pandas_edgelist(coauth2, 'source', 'target', edge_attr=['display_name', 'orcid'])
 
 nx.write_graphml_lxml(G, './notebooks/atheleteOA.graphml')
