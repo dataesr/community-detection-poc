00e1b5bd73f23782a3c250eb5668a65276f9576b
folland87 <frederic@olland.me>
2023-06-29 15:02:49 +0200

    merge

--- client/src/layout/Graph.jsx
+++ client/src/layout/Graph.jsx
@@ -1,7 +1,21 @@
 import '@react-sigma/core/lib/react-sigma.min.css';
-import { SigmaContainer } from '@react-sigma/core';
+import { useState, useEffect } from 'react';
+import { Badge, BadgeGroup, Col, Container, Row, Text, Title } from '@dataesr/react-dsfr';
+import { SigmaContainer, useRegisterEvents } from '@react-sigma/core';
 import { useQuery } from '@tanstack/react-query';
 import { UndirectedGraph } from 'graphology';
+
+const GraphEvents = ({ onNodeClick }) => {
+  const registerEvents = useRegisterEvents();
+  useEffect(() => {
+    // Register the events
+    registerEvents({
+      // node events
+      clickNode: (event) => onNodeClick(event),
+    });
+  }, [registerEvents]);
+  return null;
+};
 
 async function getScanr({ query, type }) {
   return fetch(`/api/scanr?query=${query.join(',')}&type=${type}`).then((response) => {
@@ -11,6 +25,7 @@ async function getScanr({ query, type }) {
 }
 
 export default function Graph({ counter, query, type }) {
+  const [selectedNode, setSelectedNode] = useState(null);
   const { data, isLoading } = useQuery(
     [counter],
     () => getScanr({ query, type }),
@@ -18,5 +33,78 @@ export default function Graph({ counter, query, type }) {
   );
   if (isLoading) return <div>Loading data...</div>;
   const graph = UndirectedGraph.from(data);
-  return <SigmaContainer style={{ height: '500px', backgroundColor: 'transparent !important' }} graph={graph} />;
+  const communities = graph.reduceNodes((acc, node, attr) => {
+    const { label, size, color } = attr;
+    if (!acc[color]) {
+      acc[color] = [{ id: node, label, size, degree: graph.degree(node) }];
+    } else {
+      acc[color] = [...acc[color], { id: node, label, size, degree: graph.degree(node) }].sort((a, b) => b.size - a.size);
+    }
+    return acc;
+  }, {});
+  const clustersKeys = Object.keys(communities).sort((a, b) => communities[b].length - communities[a].length).slice(0, 6);
+  return (
+    <>
+      <Container fluid className="fr-my-3w">
+        <Row gutters>
+          <Col n={selectedNode ? "8" : "12"}>
+            <SigmaContainer
+              style={{ height: '500px' }}
+              graph={graph}
+            >
+              <GraphEvents onNodeClick={(event) => setSelectedNode({ id: event.node, degree: graph.degree(event.node), ...graph.getNodeAttributes(event.node) })} />
+            </SigmaContainer>
+          </Col>
+          <Col n={selectedNode ? "4" : "0"}>
+            {selectedNode && (
+              <div className="fr-card fr-card--shadow">
+                <div className="fr-card__body">
+                  <Text bold className="fr-mb-1v">
+                    {selectedNode.label}
+                  </Text>
+                  <Text bold className="fr-mb-1v">
+                    Cluster: {selectedNode.color}
+                  </Text>
+                  <BadgeGroup>
+                    <Badge colorFamily="purple-glycine" className="fr-ml-1w" text={`${selectedNode.weight} publications`} />
+                    {graph.getNodeAttribute(selectedNode.id, 'publications')?.map((publication) => (<p>{publication}</p>))}
+                    <Badge className="fr-ml-1w" text={`${selectedNode.degree} co-autheurs`} />
+                    {graph.mapNeighbors(selectedNode.id, (node, attr) => (<p>{attr.label}</p>))}
+                    {graph.getNodeAttribute(selectedNode.id, 'keywords')?.map((keyword) => (<p>{keyword}</p>))}
+                  </BadgeGroup>
+                </div>
+              </div>
+            )}
+          </Col>
+        </Row>
+      </Container>
+      <Title as="h3">Principaux clusters</Title>
+      <Container fluid className="fr-my-3w">
+        <Row gutters>
+          {clustersKeys.map((cluster) => (
+            <Col key={cluster} n="4">
+              <div className="fr-card fr-card--shadow">
+                <p style={{ backgroundColor: cluster }}>
+                  {cluster}
+                </p>
+                <div className="fr-card__body">
+                  {communities[cluster].slice(0, 10).map((node) => (
+                    <>
+                      <Text bold className="fr-mb-1v" key={node.id}>
+                        {node.label}
+                      </Text>
+                      <BadgeGroup>
+                        <Badge colorFamily="purple-glycine" className="fr-ml-1w" text={`${node.weight} publications`} />
+                        <Badge className="fr-ml-1w" text={`${node.degree} co-autheurs`} />
+                      </BadgeGroup>
+                    </>
+                  ))}
+                </div>
+              </div>
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    </>
+  );
 }
