861b4f936122d33d8ab7f1c499560d7f1695381f
Anne L'Hôte <anne.lhote@gmail.com>
2023-06-29 10:29:47 +0200

    fix(graph): Fix structure graph

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,4 +1,4 @@
-import { Button, Container, TextInput, Title } from '@dataesr/react-dsfr';
+import { Button, Container, Title } from '@dataesr/react-dsfr';
 import { useState } from 'react';
 
 import Graph from '../layout/Graph';
@@ -8,8 +8,8 @@ export default function Home() {
   const [clicked, setClicked] = useState(false);
   const [clicked2, setClicked2] = useState(false);
   const [clicked3, setClicked3] = useState(false);
-  const [idref, setIdref] = useState('048743216');
-  const [structure, setStructure] = useState('199712586Y');
+  const [idrefs, setIdrefs] = useState(['048743216']);
+  const [structures, setStructures] = useState(['199712586Y']);
   const [tags, setTags] = useState(['athlete']);
 
   return (
@@ -29,24 +29,26 @@ export default function Home() {
         Generate graph
       </Button>
       {clicked && <Graph tags={tags} />}
-      <TextInput
-        label="Enter one or more idref separated by space"
-        value={idref}
-        onChange={(e) => setIdref(e.target.value)}
+      <TagInput
+        label="Idref"
+        hint='Validate you add by pressing "Return" key'
+        tags={idrefs}
+        onTagsChange={(_tags) => setIdrefs(_tags)}
       />
       <Button onClick={() => setClicked2(true)}>
         Generate graph for an idref
       </Button>
-      {clicked2 && <Graph idref={idref} />}
-      <TextInput
-        label="Enter one or more institution id separated by space"
-        value={structure}
-        onChange={(e) => setStructure(e.target.value)}
+      {clicked2 && <Graph idrefs={idrefs} />}
+      <TagInput
+        label="Institution id"
+        hint='Validate you add by pressing "Return" key'
+        tags={structures}
+        onTagsChange={(_tags) => setStructures(_tags)}
       />
       <Button onClick={() => setClicked3(true)}>
         Generate graph for an institution id
       </Button>
-      {clicked3 && <Graph structure={structure} />}
+      {clicked3 && <Graph structures={structures} />}
     </Container>
   );
 }
