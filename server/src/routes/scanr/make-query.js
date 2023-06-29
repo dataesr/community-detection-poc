65d169ef9b2f2650ebf0162997c9efea7d5026b9
eric <eric.jeangirard@recherche.gouv.fr>
2023-06-29 18:15:53 +0200

    sampling random in query

--- server/src/routes/scanr/make-query.js
+++ server/src/routes/scanr/make-query.js
@@ -1,15 +1,22 @@
 const DEFAULT_YEARS = [2018, 2019, 2020, 2021, 2022, 2023];
-const DEFAULT_SIZE = 10000;
+const DEFAULT_SIZE = 5000;
 
 export const makeQueryByAuthor = (query, size = DEFAULT_SIZE, years = DEFAULT_YEARS) => ({
   size,
+  _source: ['id', 'authors', 'domains'],
   query: {
-    bool: {
-      filter: [
-        { terms: { 'authors.role.keyword': ['author', 'directeurthese'] } },
-        { terms: { year: years } },
-        { terms: { 'authors.person.id.keyword': query.split(',').map((id) => `idref${id}`) } },
-      ],
+    function_score: {
+      query: {
+        bool: {
+          filter: [
+            { terms: { 'authors.role.keyword': ['author', 'directeurthese'] } },
+            { terms: { year: years } },
+            { terms: { 'authors.person.id.keyword': query.split(',').map((id) => `idref${id}`) } },
+          ],
+        },
+      },
+      random_score: { seed: 2001 },
+      boost_mode: 'replace',
     },
   },
 });
@@ -18,46 +25,59 @@ export const makeQueryByKeyword = (query, size = DEFAULT_SIZE, years = DEFAULT_Y
   size,
   _source: ['id', 'authors', 'domains'],
   query: {
-    bool: {
-      filter: [
-        { terms: { 'authors.role.keyword': ['author', 'directeurthese'] } },
-        { terms: { year: years } },
-      ],
-      must: {
-        query_string: {
-          fields: [
-            'title.default',
-            'title.fr',
-            'title.en',
-            'keywords.en',
-            'keywords.fr',
-            'keywords.default',
-            'domains.label.default',
-            'domains.label.fr',
-            'domains.label.en',
-            'summary.default',
-            'summary.fr',
-            'summary.en',
-            'alternativeSummary.default',
-            'alternativeSummary.fr',
-            'alternativeSummary.en',
+    function_score: {
+      query: {
+        bool: {
+          filter: [
+            { terms: { 'authors.role.keyword': ['author', 'directeurthese'] } },
+            { terms: { year: years } },
           ],
-          query: query.split(',').map((q) => `"${q}"`).join(' '),
+          must: {
+            query_string: {
+              fields: [
+                'title.default',
+                'title.fr',
+                'title.en',
+                'keywords.en',
+                'keywords.fr',
+                'keywords.default',
+                'domains.label.default',
+                'domains.label.fr',
+                'domains.label.en',
+                'summary.default',
+                'summary.fr',
+                'summary.en',
+                'alternativeSummary.default',
+                'alternativeSummary.fr',
+                'alternativeSummary.en',
+              ],
+              query: query.split(',').map((q) => `"${q}"`).join(' '),
+            },
+          },
         },
       },
+      random_score: { seed: 2001 },
+      boost_mode: 'replace',
     },
   },
 });
 
 export const makeQueryByStructure = (query, size = DEFAULT_SIZE, years = DEFAULT_YEARS) => ({
   size,
+  _source: ['id', 'authors', 'domains'],
   query: {
-    bool: {
-      filter: [
-        { terms: { 'authors.role.keyword': ['author', 'directeurthese'] } },
-        { terms: { year: years } },
-        { terms: { 'affiliations.id.keyword': query.split(',') } },
-      ],
+    function_score: {
+      query: {
+        bool: {
+          filter: [
+            { terms: { 'authors.role.keyword': ['author', 'directeurthese'] } },
+            { terms: { year: years } },
+            { terms: { 'affiliations.id.keyword': query.split(',') } },
+          ],
+        },
+      },
+      random_score: { seed: 2001 },
+      boost_mode: 'replace',
     },
   },
 });
