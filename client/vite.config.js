3a92a36a31f71ad9a66b740f8e82b045b209e3eb
Anne L'Hôte <anne.lhote@gmail.com>
2023-08-22 19:16:29 +0200

    typo

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { 
+  server: {
     proxy: {
       '/api': {
         target: 'http://localhost:3000',
@@ -13,5 +13,5 @@ export default defineConfig({
         ws: true,
       },
     },
-  }
-})
+  },
+});
