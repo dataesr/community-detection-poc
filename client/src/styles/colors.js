a44c3fc000f341cfce8754cda34259536f20f5f7
victor barbier <victor.barbier@recherche.gouv.fr>
2023-10-27 16:22:51 +0200

    feat(colors): remove grey and add new colors

--- client/src/styles/colors.js
+++ client/src/styles/colors.js
@@ -1,10 +1,10 @@
 export const DEFAULT_NODE_COLOR = '#7b7b7b';
 export const COMMUNTIY_COLORS = [
-  '#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f',
-  '#bcbd22', '#17becf', '#aec7e8', '#ffbb78', '#98df8a', '#ff9896', '#c5b0d5', '#c49c94',
-  '#f7b6d2', '#c7c7c7', '#dbdb8d', '#9edae5', '#b3e2cd', '#fddaec', '#c7e9c0', '#fdae6b',
-  '#b5cf6b', '#ce6dbd', '#dadaeb', '#393b79', '#637939', '#8c6d31', '#843c39', '#ad494a',
-  '#d6616b', '#e7ba52', '#e7cb94', '#843c39', '#ad494a', '#d6616b', '#e7969c', '#7b4173',
-  '#a55194', '#ce6dbd', '#de9ed6', '#3182bd', '#6baed6', '#9ecae1', '#c6dbef', '#e6550d',
-  '#fd8d3c', '#fdae6b', '#fdd0a2', '#31a354',
+  '#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#bcbd22', '#17becf', '#66c2a5',
+  '#fc8d62', '#8da0cb', '#e78ac3', '#a6d854', '#ffd92f', '#e5c494', '#aec7e8', '#ffbb78', '#98df8a', '#ff9896',
+  '#c5b0d5', '#c49c94', '#386df9', '#12c8e6', '#5af8c8', '#a4f89f', '#ecc86f', '#ff6d38', '#f7b6d2', '#dbdb8d',
+  '#9edae5', '#b3e2cd', '#fddaec', '#c7e9c0', '#fdae6b', '#b5cf6b', '#ce6dbd', '#393b79', '#637939', '#8c6d31',
+  '#843c39', '#ad494a', '#d6616b', '#e7ba52', '#e7cb94', '#843c39', '#ad494a', '#d6616b', '#e7969c', '#7b4173',
+  '#a55194', '#ce6dbd', '#de9ed6', '#3182bd', '#6baed6', '#9ecae1', '#c6dbef', '#e6550d', '#fd8d3c', '#fdae6b',
+  '#fdd0a2', '#31a354', '#74c476', '#a1d99b', '#c7e9c0', '#756bb1', '#9e9ac8', '#bcbddc', '#dadaeb',
 ];
